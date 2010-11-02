ddc3ff06c8b02b2dbaf7a4cc5e564da83f830c7f
Arnaud Rebts <arnaud.rebts@gmail.com>
2010-11-02 23:05:56 +0100

    Add Ncqrs dlls and a first aggreate root, Member

---
+++ src/CqrsLearning.Domain/Member.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CqrsLearning.Events.Member;
+using Ncqrs.Domain;
+
+namespace CqrsLearning.Domain
+{
+    public class Member : AggregateRootMappedByConvention
+    {
+        private long nsi;
+        private string firstName;
+        private string lastName;
+        private DateTime createdAt;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Member"/> class.
+        /// </summary>
+        /// <param name="nsi">The nsi.</param>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        public Member(long nsi, string firstName, string lastName)
+        {
+            this.ApplyEvent(new MemberCreatedEvent
+            {
+                Nsi = nsi,
+                FirstName = firstName,
+                LastName = lastName,
+                CreatedAt = DateTime.UtcNow
+            });
+        }
+
+        protected virtual void OnMemberCreated(MemberCreatedEvent evnt)
+        {
+            this.nsi = evnt.Nsi;
+            this.firstName = evnt.FirstName;
+            this.lastName = evnt.LastName;
+            this.createdAt = evnt.CreatedAt;
+        }
+    }
+}
---
+++ src/CqrsLearning.Events/Member/MemberCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Ncqrs.Eventing.Sourcing;
+
+namespace CqrsLearning.Events.Member
+{
+    public class MemberCreatedEvent : SourcedEvent
+    {
+        public long Nsi { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
